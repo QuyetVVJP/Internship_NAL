58dde18608385bec7ea3ef36ca1cf52af98e0a6b
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-17 21:43:43 +0900

    FE: Routing and navigation

--- FE_Manage_Device/src/app/app-routing.module.ts
+++ FE_Manage_Device/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeviceListComponent } from './device/device-list/device-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list-device', component:DeviceListComponent},
+  {path: '', redirectTo:'list-device', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
