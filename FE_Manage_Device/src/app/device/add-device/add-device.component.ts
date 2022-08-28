e4da74c5a7f1ece32bc7d7080e1fcd5191e805be
nghia <c210190@ccg.ac.jp>
2022-08-28 15:43:08 +0900

    CRUD-device - 1

---
+++ FE_Manage_Device/src/app/device/add-device/add-device.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+//import { error } from 'console';
+import { Device } from '../device';
+import { DeviceService } from '../device.service';
+
+@Component({
+  selector: 'app-add-device',
+  templateUrl: './add-device.component.html',
+  styleUrls: ['./add-device.component.css']
+})
+export class AddDeviceComponent implements OnInit {
+
+  device= new Device();
+  constructor(private deviceService: DeviceService,
+    private router: Router
+   ) { }
+
+  ngOnInit(): void {
+
+  }
+  savedevice(){
+    this.deviceService.addDevice(this.device).subscribe(data =>{
+        console.log(data);
+    },
+    error => console.log(error));
+  }
+  gotolistDevices(){
+    this.router.navigate(['/device']);
+  }
+   onSubmit(){
+    console.log(this.device);
+    this.savedevice();
+   }
+}
