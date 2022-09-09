ee314f45a97bb3df5c2abc9558378e480be20489
Khang Sparky <smileykiller97@gmail.com>
2022-09-10 00:05:14 +0700

    update query Search_Pagination_USER

--- FE_Manage_Device/src/app/user/update-user/update-user.component.ts
+++ FE_Manage_Device/src/app/user/update-user/update-user.component.ts
@@ -32,25 +32,30 @@ export class UpdateUserComponent implements OnInit {
   public onImageUpload(event) {
     this.uploadedImage = event.target.files[0];
   }
-    goToListUsers(){
-      this.router.navigate(['/user-list']);
+  saveUser(){
+    this.userService.updateUser(this.id, this.user).subscribe(data =>{
+        console.log(data);
+        this.goToListUsers();
+    },
+    error => console.log(error));
+  }
+
+  goToListUsers(){
+    this.router.navigate(['/list-device/user-list']);
+  }
+  onSubmit(){
+    console.log(this.user);
+    this.saveUser();
     }
-    onSubmit(){
-      this.userService.updateUser(this.id, this.user).subscribe(data =>{
-        this.imageUploadAction(this.id);
-        
-         
-      },error => console.log(error));
-    }
-    imageUploadAction(id: number) {
+  imageUploadAction(id: number) {
       const imageFormData = new FormData();
       imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
-      
+
       this.httpClient.post('http://localhost:8080/users/upload/image/'+id, imageFormData, {  responseType: 'text'  })
         .subscribe((response) => {
           this.goToListUsers();
       },error => console.log(error)
-        
+
         );
       }
   }
--- src/main/java/com/example/manage_device/repository/UserRepository.java
+++ src/main/java/com/example/manage_device/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.manage_device.repository;
 
-import com.example.manage_device.model.Device;
-import com.example.manage_device.model.User;
+import com.example.manage_device.exception.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -32,6 +29,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM user WHERE is_login = 1" , nativeQuery = true)
     User findUserLogin();
 
-    @Query(value = "SELECT * FROM device WHERE device_name LIKE %:term% OR os LIKE %:term%" , nativeQuery = true)
+    @Query(value = "SELECT * FROM user WHERE first_name LIKE %:term% OR last_name LIKE %:term% OR email LIKE %:term% OR employee_id LIKE %:term% OR phone LIKE %:term%" , nativeQuery = true)
     public Page<User> searchByKeyword(@Param("term") String term, Pageable paging);
 }
