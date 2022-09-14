e54031f38eadb377c2ced3e0871c278883ba08a6
user <c210121@ccg.ac.jp>
2022-09-15 00:36:29 +0900

    Update Search pagination 9/15

--- src/main/java/com/example/manage_device/repository/DeviceLoanRepository.java
+++ src/main/java/com/example/manage_device/repository/DeviceLoanRepository.java
@@ -2,6 +2,7 @@ package com.example.manage_device.repository;
 
 import com.example.manage_device.model.Device;
 import com.example.manage_device.model.DeviceLoan;
+import com.example.manage_device.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +12,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeviceLoanRepository extends JpaRepository<DeviceLoan, Long> {
-    @Query(value = "SELECT * FROM device WHERE device_name LIKE %:term% OR os LIKE %:term%" , nativeQuery = true)
+    @Query(value = "SELECT user.first_name, user.last_name, device.device_name, user.email , device_loan.status, device_loan.borrow_date, device_loan.return_date" +
+            "  FROM device_loan" +
+            "  INNER JOIN user" +
+            "  ON device_loan.user_id = user.id" +
+            "  INNER JOIN device" +
+            "  ON device_loan.device_id = device.id  LIKE %:term%" , nativeQuery = true)
     public Page<DeviceLoan> searchByKeyword(@Param("term") String term,
                                         Pageable paging);
+
 }
